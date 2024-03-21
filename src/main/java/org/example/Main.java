1e475a4e6a573320c2b13674f60cb6e3b7344876
kimhyeji <gpwl5589@gmail.com>
2024-03-21 14:27:32 +0900

    조회수 기능 추가, 상세, 리스트 출력 자리 수 지정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -46,11 +46,11 @@ public class Main {
                     continue;
                 }
 
-                System.out.println("번호 | 제목");
+                System.out.println("번호 | 조회 | 제목 ");
                 for ( int i = articles.size() - 1; i >= 0 ; i-- ) {
                     Article article = articles.get(i);
 
-                    System.out.printf("%d    | %s\n", article.id, article.title);
+                    System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
                 }
             }
             else if ( cmd.startsWith("article detail ") ) {
@@ -73,10 +73,13 @@ public class Main {
                     continue;
                 }
 
+                foundArticle.increaseHit();
+
                 System.out.printf("번호 : %d\n", foundArticle.id);
                 System.out.printf("날짜 : %s\n", foundArticle.regDate);
                 System.out.printf("제목 : %s\n", foundArticle.title);
                 System.out.printf("내용 : %s\n", foundArticle.body);
+                System.out.printf("조회 : %d\n", foundArticle.hit);
             }
             else if ( cmd.startsWith("article modify ") ) {
                 String[] cmdBits = cmd.split(" ");
@@ -150,11 +153,17 @@ class Article {
     String regDate;
     String title;
     String body;
+    int hit;
 
     public Article(int id, String regDate, String title, String body) {
         this.id = id;
         this.regDate = regDate;
         this.title = title;
         this.body = body;
+        this.hit = 0;
+    }
+
+    public void increaseHit() {
+        hit++;
     }
 }
