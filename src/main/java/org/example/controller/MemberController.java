35e50d430e15f091379c7bf30eaf999385d2cbe6
kimhyeji <gpwl5589@gmail.com>
2024-03-22 18:45:42 +0900

    게시물 작성 시 로그인 된 회원이 작성자가 됨, 리스트, 상세에 작성자 표시

--- src/main/java/org/example/controller/MemberController.java
+++ src/main/java/org/example/controller/MemberController.java
@@ -13,7 +13,6 @@ public class MemberController extends Controller {
     private List<Member> members;
     private String cmd;
     private String actionMethodName;
-    private Member loginedMember;
 
     public MemberController(Scanner sc) {
         this.sc = sc;
@@ -118,10 +117,6 @@ public class MemberController extends Controller {
 
         loginedMember = member;
         System.out.printf("로그인 성공! %s님 환영합니다!\n", loginedMember.name);
-    }
-
-    private boolean isLogined() {
-        return loginedMember != null;
     }
 
     private void doLogout() {
