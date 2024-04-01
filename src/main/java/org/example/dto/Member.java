5a0aac7bd8d7ea289c92d9e11cb8591d478bc848
kimhyeji <gpwl5589@gmail.com>
2024-04-01 18:24:32 +0900

    멤버의 모든 데이터들을 db에서 추출

--- src/main/java/org/example/dto/Member.java
+++ src/main/java/org/example/dto/Member.java
@@ -12,9 +12,7 @@ public class Member extends Dto {
     public String loginPw;
     public String name;
 
-    public Member(int id, String regDate, String loginId, String loginPw, String name) {
-        this.id = id;
-        this.regDate = regDate;
+    public Member(String loginId, String loginPw, String name) {
         this.loginId = loginId;
         this.loginPw = loginPw;
         this.name = name;
