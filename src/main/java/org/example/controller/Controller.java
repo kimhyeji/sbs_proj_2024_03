315a47e34484e2377a75ca3ba72ce0e84f2f6af9
kimhyeji <gpwl5589@gmail.com>
2024-04-01 15:14:43 +0900

    DB커넥션 추가 후 DB설정

--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -3,7 +3,6 @@ package org.example.controller;
 import org.example.dto.Member;
 
 public abstract class Controller {
-
     public abstract void doAction(String cmd, String actionMethodName);
     public abstract void makeTestData();
 }
