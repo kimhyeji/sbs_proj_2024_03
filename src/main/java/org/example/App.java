de51e2b2758c6e7872450ca72d0122193a37682c
kimhyeji <gpwl5589@gmail.com>
2024-04-01 14:44:22 +0900

    로그인 관련 정보를 controller에서 전문적으로 하는 session에게 옮김

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.container.Container;
 import org.example.controller.ArticleController;
 import org.example.controller.Controller;
 import org.example.controller.ExportController;
@@ -66,7 +67,7 @@ public class App {
                 case "article/delete":
                 case "article/modify":
                 case "memeber/logout":
-                    if ( Controller.isLogined() == false ) {
+                    if ( Container.getSession().isLogined() == false ) {
                         System.out.println("로그인 후 이용해주세요.");
                         continue;
                     }
@@ -76,7 +77,7 @@ public class App {
             switch ( actionName ) {
                 case "member/login":
                 case "memeber/join":
-                    if ( Controller.isLogined() ) {
+                    if (Container.getSession().isLogined() ) {
                         System.out.println("로그아웃 후 이용해주세요.");
                         continue;
                     }
