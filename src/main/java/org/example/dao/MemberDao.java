d12b79605fb0634105a4cba816d6a3ecd9b209ce
kimhyeji <gpwl5589@gmail.com>
2024-04-02 18:04:56 +0900

    row값에 대해서 예외처리 해줌

--- src/main/java/org/example/dao/MemberDao.java
+++ src/main/java/org/example/dao/MemberDao.java
@@ -23,8 +23,8 @@ public class MemberDao extends Dao {
         sb.append(String.format("INSERT INTO `member` "));
         sb.append(String.format("SET regDate = NOW(), "));
         sb.append(String.format("updateDate = NOW(), "));
-        sb.append(String.format("loginId = '%s' ", member.loginId));
-        sb.append(String.format("loginPw = '%s' ", member.loginPw));
+        sb.append(String.format("loginId = '%s', ", member.loginId));
+        sb.append(String.format("loginPw = '%s', ", member.loginPw));
         sb.append(String.format("`name` = '%s' ", member.name));
 
         return dbConnection.insert(sb.toString());
@@ -39,6 +39,10 @@ public class MemberDao extends Dao {
         sb.append(String.format("WHERE loginId = '%s' ", loginId));
 
         Map<String, Object> memberRow = dbConnection.selectRow((sb.toString()));
+
+        if ( memberRow.isEmpty() ) {
+            return null;
+        }
 
         return new Member(memberRow);
     }
